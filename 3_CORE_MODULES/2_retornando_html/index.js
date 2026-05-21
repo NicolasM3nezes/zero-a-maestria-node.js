31969452236ff192f0fef04fb50821ef8f9b54e4
Nicolas Menezes <nicolas.seabra09@gmail.com>
2026-05-21 15:02:52 -0300

    Aula sobre Core

---
+++ 3_CORE_MODULES/2_retornando_html/index.js
@@ -0,0 +1,16 @@
+
+const http = require('http')
+
+const port = 3000
+
+const server = http.createServer((req, res) => {
+
+    res.statusCode = 200
+    res.setHeader('Contenty-Type', 'text/html')
+    res.end('<h1>Olá, este e meu primeiro servidor com HTML </h1><p>Testando atualização</p>')
+
+})
+
+server.listen(port,() => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
